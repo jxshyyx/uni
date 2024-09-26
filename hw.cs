2c3f0772c2bcd61cd1a03344f32b1f4ab6070a6e
jxshyyx <119676265+jxshyyx@users.noreply.github.com>
2024-09-26 14:33:45 +0100

    Add files via upload

---
+++ hw.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+using System.Xml.Linq;
+
+// This is the mils to kilometres homework
+//namespace Firstapplication
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter the distance in miles:");
+//            string input = Console.ReadLine();
+
+//            if (double.TryParse(input, out double miles))
+//            {
+//                double kilometers = miles * 1.60934;
+//                Console.WriteLine($"{miles} miles is equal to {kilometers} kilometers.");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Please enter a valid number.");
+//            }
+//        }
+//    }
+//}
+
+// this is the basic calculator 
+    //{
+    //    Console.WriteLine("Welcome to the Basic Calculator!");
+
+    //    // asks the user for the first number
+    //    Console.Write("Enter the first number: ");
+    //    double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+    //    // asks the user for the second number
+    //    Console.Write("Enter the second number: ");
+    //    double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+    //    // These are the actions that will make the calculator do these operations
+    //    double addition = firstNumber + secondNumber;
+    //    double subtraction = firstNumber - secondNumber;
+    //    double multiplication = firstNumber * secondNumber;
+    //    double division = secondNumber != 0 ? firstNumber / secondNumber : double.NaN; //  division by zero is what NaN means ( google has helped alot with this homework)
+
+    //    // These are what displays the results to the user
+    //    Console.WriteLine($"Addition: {firstNumber} + {secondNumber} = {addition}");
+    //    Console.WriteLine($"Subtraction: {firstNumber} - {secondNumber} = {subtraction}");
+    //    Console.WriteLine($"Multiplication: {firstNumber} * {secondNumber} = {multiplication}");
+        
+    //    if (secondNumber != 0)
+    //    {
+    //        Console.WriteLine($"Division: {firstNumber} / {secondNumber} = {division}");
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Division: Cannot divide by zero.");
+    //    }
+    //}
+
+
+
+
+
+
+
